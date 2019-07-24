c6125b326175cc873056cd21160a1d8c8797bcb5
SparoJ <sdhchn@gmail.com>
2019-07-25 00:07:22 +0800

    data structure : [LinkedList] leetcode 删除节点【重复元素1 问题 遍历和递归方案】

---
+++ JavaPractice/src/com/sparo/leetcode/node/DeleteElementNode.java
@@ -0,0 +1,12 @@
+package com.sparo.leetcode.node;
+
+/**
+ * description:
+ * Created by sdh on 2019-07-23
+ */
+public class DeleteElementNode {
+
+
+
+
+}
--- JavaPractice/src/com/sparo/util/Utils.java
+++ JavaPractice/src/com/sparo/util/Utils.java
@@ -9,4 +9,8 @@ public class Utils {
     public static void println(Object o) {
         System.out.println(o);
     }
+
+    public static void print(Object o) {
+        System.out.print(o);
+    }
 }
