37a329e56c358173da13e74b18d0b1c55e570cf9
SparoJ <sdhchn@gmail.com>
2019-12-18 00:05:38 +0800

    data structure : [util] 补充工具类打印能力

--- JavaPractice/src/com/sparo/util/Utils.java
+++ JavaPractice/src/com/sparo/util/Utils.java
@@ -10,6 +10,18 @@ public class Utils {
         System.out.println(o);
     }
 
+    @SuppressWarnings("unchecked")
+    public static void printIntArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i!=arr.length-1) {
+                System.out.print(arr[i]+"->");
+            } else {
+                System.out.print(arr[i]);
+            }
+        }
+        System.out.println("------------------------");
+    }
+
     public static void print(Object o) {
         System.out.print(o);
     }
