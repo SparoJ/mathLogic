d0ba5b9b13c303da401f5ef0727948bb55cff6c7
SparoJ <sdhchn@gmail.com>
2020-02-08 21:22:55 +0800

    data structure : [算法-排序算法] 排序测试入口类修改

--- JavaPractice/src/com/sparo/math/App.java
+++ JavaPractice/src/com/sparo/math/App.java
@@ -1,5 +1,7 @@
 package com.sparo.math;
 
+import com.sparo.util.Utils;
+
 /**
  * description: 排序算法
  * Created by sdh on 2019-03-27
@@ -10,13 +12,45 @@ public class App {
         int[] arr = {3, 3, 6, 7 , 4, 3, 9, 10, 2 , 3, 1, 5, 0, 8};
 
         // 测试插入排序
-        IAlgorithmStrategy strategy = new InsertSortStrategy();
+//        IAlgorithmStrategy strategy = new InsertSortStrategy();
 
 //        QuickSortStrategy strategy = new QuickSortStrategy();
-//        QuickSortStrategyPlus strategy = new QuickSortStrategyPlus();
+        QuickSortStrategyPlus strategy = new QuickSortStrategyPlus();
+        strategy.sort(arr);
 
         App app = new App();
-        app.testAlgorithm(strategy, arr);
+//        app.testAlgorithm(strategy, arr);
+        // 测试异或使用
+//        app.test();
+
+        //shell 测试
+//        ShellSortStrategy sss = new ShellSortStrategy();
+//        sss.insertSort(arr);
+
+        //bubble 测试
+//        BubbleSortStrategy bss = new BubbleSortStrategy();
+//        bss.sort(arr);
+
+        //select 测试
+//        SelectSortStrategy selectS = new SelectSortStrategy();
+//        selectS.sort(arr);
+
+        // merge 测试
+//        MergeSortStrategy mss = new MergeSortStrategy();
+//        mss.sort(arr);
+        Utils.printIntArray(arr);
+    }
+
+    /**
+     * 测试异或使用
+     */
+    public void test() {
+        int a = 3;
+        int b = 5;
+        b = a^b;
+        a = a^b;
+        b = a^b;
+        System.out.print("a->" + a + "b->" + b);
     }
 
     public void testAlgorithm(IAlgorithmStrategy strategy, int[] arr) {
