9f8977aad4ed2638f2db34cbd5cdd09e51f49153
SparoJ <sdhchn@gmail.com>
2020-03-01 16:23:28 +0800

    data structure : [算法-topK] 快排在topK中的应用

---
+++ JavaPractice/src/com/sparo/math/base/TopK.java
@@ -0,0 +1,59 @@
+package com.sparo.math.base;
+
+import com.sparo.math.QuickSortStrategyPlus;
+import com.sparo.util.Utils;
+
+/**
+ * description:
+ * Created by sdh on 2020-02-07
+ */
+public class TopK {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 6, 7 , 4, 3, 9, 10, 2 , 3, 1, 5, 0, 8};
+        TopK tk = new TopK();
+       int k =  tk.quickSelect(arr, 0, arr.length-1, 5);
+       System.out.println("topK->" + k);
+       int[] sort = arr.clone();
+        QuickSortStrategyPlus qssp = new QuickSortStrategyPlus();
+        Utils.printIntArray(qssp.sort(sort));
+
+//        0->1->2->3->3->3->3 ->4->5->6->7->8->9->10-
+    }
+
+    int Partition(int A[], int left, int right)
+    {
+        int temp = A[left]; //将A[left]存放至临时变量temp
+        while(left<right)  //只要left与right不相遇
+        {
+            while(left<right && A[right]>temp) //反复左移right
+                right--;
+            A[left] = A[right];	//将A[right]挪到A[left]
+            while(left<right && A[left]<=temp)	 //反复右移left
+                left++;
+            A[right] = A[left];	//将A[left]挪到A[right]
+        }
+
+        A[left] = temp;	//把temp放到left与right相遇的地方
+        return left;	//返回相遇的下标
+    }
+
+    //快速选择算法，从A[left,right]中返回第k大的数
+    int quickSelect(int A[], int left, int right, int k)
+    {
+        if(left==right)
+            return A[left];	//边界
+
+        int p = Partition(A, left, right);	//划分后主元的位置为p
+
+        int m = p-left+1;	//A[p]是A[left,right]中的第M大
+
+        if(k==m)	//找到第k大的数
+            return A[p];
+        if(k<m)		//第k大的数在主元左侧
+            return quickSelect(A, left, p-1, k);		//往主元左侧找第k大
+        else		//第k大的数在主元右侧
+            return quickSelect(A, p+1, right, k-m);	//往主元右侧找第k-m大
+    }
+
+}
