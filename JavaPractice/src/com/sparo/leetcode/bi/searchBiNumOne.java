1f4fe2cfdf595348299a26803d3bda9a79574504
SparoJ <sdhchn@gmail.com>
2020-07-01 23:19:43 +0800

    data structure : leetcode 二分搜索 左右边界标准模版代码使用 leetcode 34

--- JavaPractice/src/com/sparo/leetcode/bi/searchBiNumOne.java
+++ JavaPractice/src/com/sparo/leetcode/bi/searchBiNumOne.java
@@ -5,6 +5,21 @@ package com.sparo.leetcode.bi;
  * https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof/
  * Created by sdh on 2020-07-01
  */
+
+/**
+ * 统计一个数字在排序数组中出现的次数。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: nums = [5,7,7,8,8,10], target = 6
+ * 输出: 0
+ */
 public class searchBiNumOne {
 
     public int search(int[] nums, int target) {
