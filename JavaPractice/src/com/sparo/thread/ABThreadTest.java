b417be19e9d4dd3d278610811ad302784ee896e1
SparoJ <sdhchn@gmail.com>
2020-08-18 09:30:19 +0800

    data structure : 奇偶线程打印/ABC顺序打印问题

--- JavaPractice/src/com/sparo/thread/ABThreadTest.java
+++ JavaPractice/src/com/sparo/thread/ABThreadTest.java
@@ -5,20 +5,21 @@ package com.sparo.thread;
  * Created by sdh on 2019-12-01
  */
 public class ABThreadTest {
-    public static int a = 0;
-    public static final Runnable RUNNABLE = new Runnable() {
-        public void run() {
-            a++;
-            System.out.println(a);
-        }
-    };
+    public  int a = 0;
+    private final Object monitor = new Object();
+//    public static final Runnable RUNNABLE = new Runnable() {
+//        public void run() {
+//            a++;
+//            System.out.println(a);
+//        }
+//    };
 
     public static void main(String[] args) {
 //        testErrorMethod();
         ABThreadTest test = new ABThreadTest();
-//        test.testOddEven();
-//        test.testMultiThread(3);
-        test. obtainStr();
+        test.testOddEven();
+//        test.testMultiThread(2);
+//        test. obtainStr();
     }
 
     public void obtainStr() {
@@ -46,14 +47,19 @@ public class ABThreadTest {
         }
     }
 
+    static String[] abcArr = {"A", "B", "C"};
+    static int threadCount = 4;
+
     public Runnable oddEvenRun = new Runnable() {
         public void run() {
             try {
                 while(a<=99) {
-                    synchronized (RUNNABLE) {
-                        RUNNABLE.notify();
+                    synchronized (monitor) {
+
+                        monitor.notify();
+//                        System.out.println("thread count number::" + (abcArr[(a-1)%abcArr.length]) + "-->>thread name::" + Thread.currentThread().getName());
                         System.out.println("thread count number::" + (a++) + "-->>thread name::" + Thread.currentThread().getName());
-                        RUNNABLE.wait();
+                        monitor.wait();
                     }
                 }
             } catch (InterruptedException e) {
@@ -64,28 +70,28 @@ public class ABThreadTest {
 
 
 
-    private static void testErrorMethod() {
-        Thread threadA = new Thread(RUNNABLE);
-        Thread threadB = new Thread(RUNNABLE);
-        try {
-            while (a<=99) {
-                /**
-                 * IllegalThreadStateException java 线程不能重复start 否则 抛出异常
-                 * if (threadStatus != 0)
-                 *             throw new IllegalThreadStateException();
-                 *
-                 *   解决： 即循环处理   start在循环外层，给thread 传入runnable
-                 */
-
-                threadA.start();
-                threadA.join();
-                threadB.start();
-                threadB.join();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    private static void testErrorMethod() {
+//        Thread threadA = new Thread(RUNNABLE);
+//        Thread threadB = new Thread(RUNNABLE);
+//        try {
+//            while (a<=99) {
+//                /**
+//                 * IllegalThreadStateException java 线程不能重复start 否则 抛出异常
+//                 * if (threadStatus != 0)
+//                 *             throw new IllegalThreadStateException();
+//                 *
+//                 *   解决： 即循环处理   start在循环外层，给thread 传入runnable
+//                 */
+//
+//                threadA.start();
+//                threadA.join();
+//                threadB.start();
+//                threadB.join();
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 
 }
